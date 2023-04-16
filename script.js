c25c65950a334860cacdd1f34a3c41985d7856d7
kevincallaghan <kevincallaghan5@yahoo.com>
2023-04-16 14:49:40 -0500

    passed City to the API and retrieved Lat and Lon

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ function handleSearchFormSubmit(event) {
   event.preventDefault();
 
   var searchInputVal = document.querySelector('#search-input').value;
+  localStorage.setItem("searchInputVal", searchInputVal);
 
   if (!searchInputVal) {
     console.error('You need a search input value!');
--- searchResults.js
+++ searchResults.js
@@ -1,123 +1,46 @@
-var resultTextEl = document.querySelector('#result-text');
-var resultContentEl = document.querySelector('#result-content');
-var searchFormEl = document.querySelector('#search-form');
+// Get the API key from a variable
+const apiKey = '8bfd53837493b8bbb4ace442550cacf8';
+console.log(apiKey);
+// Get the city from local storage that was saved in the form submit on index.html
+const city = localStorage.getItem("searchInputVal");
+console.log(city);
+// Build the fetch URL using the API key and city
+const queryUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&contentType=json`;
 
-searchFormEl.addEventListener('submit', function (event) {
-  event.preventDefault();
+// Make the fetch request
+fetch(queryUrl, {
+  method: 'GET',
+  headers: {
 
-  var searchInputVal = document.querySelector('#search-input').value;
+  },
 
-  if (!searchInputVal) {
-    console.error('You need a search input value!');
-    return;
+}).then(response => {
+  if (!response.ok) {
+    throw response;
   }
 
-  var locQueryUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + searchInputVal + '&appid=8bfd53837493b8bbb4ace442550cacf8';
+  return response.json();
 
-  fetch(locQueryUrl)
-    .then(function (response) {
-      if (!response.ok) {
-        throw response.json();
-      }
+}).then(response => {
+  processWeatherData(response);
 
-      return response.json();
+}).catch((errorResponse) => {
+  if (errorResponse.text) {
+    errorResponse.text().then(errorMessage => {
+      console.error(errorMessage); // logs the error message
     })
-    .then(function (data) {
-      console.log(data);
-    })
-    .catch(function (error) {
-      console.error(error);
-    });
+  } else {
+    console.error(errorResponse); // logs the error response object
+  }
 });
 
+function processWeatherData(response) {
 
-// function printResults(resultObj) {
-//   console.log(resultObj);
+  var location = response.resolvedAddress;
+  var latitude = response.coord.lat;
+  var longitude = response.coord.lon;
+  console.log(latitude);
+  console.log(longitude);
+}
 
-//   // set up `<div>` to hold result content
-//   var resultCard = document.createElement('div');
-//   resultCard.classList.add('card', 'bg-light', 'text-dark', 'mb-3', 'p-3');
-
-//   var resultBody = document.createElement('div');
-//   resultBody.classList.add('card-body');
-//   resultCard.append(resultBody);
-
-//   var titleEl = document.createElement('h3');
-//   titleEl.textContent = resultObj.title;
-
-//   var bodyContentEl = document.createElement('p');
-//   bodyContentEl.innerHTML =
-//     '<strong>Date:</strong> ' + resultObj.date + '<br/>';
-
-//   if (resultObj.subject) {
-//     bodyContentEl.innerHTML +=
-//       '<strong>Subjects:</strong> ' + resultObj.subject.join(', ') + '<br/>';
-//   } else {
-//     bodyContentEl.innerHTML +=
-//       '<strong>Subjects:</strong> No subject for this entry.';
-//   }
-
-//   if (resultObj.description) {
-//     bodyContentEl.innerHTML +=
-//       '<strong>Description:</strong> ' + resultObj.description[0];
-//   } else {
-//     bodyContentEl.innerHTML +=
-//       '<strong>Description:</strong>  No description for this entry.';
-//   }
-
-//   var linkButtonEl = document.createElement('a');
-//   linkButtonEl.textContent = 'Read More';
-//   linkButtonEl.setAttribute('href', resultObj.url);
-//   linkButtonEl.classList.add('btn', 'btn-dark');
-
-//   resultBody.append(titleEl, bodyContentEl, linkButtonEl);
-
-//   resultContentEl.append(resultCard);
-// }
-
-// //=================================================
-
-// function handleSearchFormSubmit(event) {
-//   event.preventDefault();
-
-//   var searchInputVal = document.querySelector('#search-input').value;
-
-//   if (!searchInputVal) {
-//     console.error('You need a search input value!');
-//     return;
-//   }
-
-//   let locQueryUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + searchInputVal + '&appid=8bfd53837493b8bbb4ace442550cacf8';
-
-//   fetch(locQueryUrl)
-//     .then(function (response) {
-//       if (!response.ok) {
-//         throw response.json();
-//       }
-
-//       return response.json();
-//     })
-//     .then(function (locRes) {
-//       // write query to page so user knows what they are viewing
-//       resultTextEl.textContent = searchInputVal;
-
-//       console.log(locRes);
-
-//       if (!locRes.results.length) {
-//         console.log('No results found!');
-//         resultContentEl.innerHTML = '<h3>No results found, search again!</h3>';
-//       } else {
-//         resultContentEl.textContent = '';
-//         for (var i = 0; i < locRes.results.length; i++) {
-//           printResults(locRes.results[i]);
-//         }
-//       }
-//     })
-//     .catch(function (error) {
-//       console.error(error);
-//     });
-// }
-// var searchButtonEl = document.querySelector('#search-button');
-// searchButtonEl.addEventListener('click', handleSearchFormSubmit);
-
-// getParams();
+//! SOURCE: I used documentation for a different weather API to get source code and plug in my query and API Key and then return the data I need.  Here is the documentation I used: https://www.visualcrossing.com/resources/documentation/weather-api/how-to-load-weather-data-in-javascript/
